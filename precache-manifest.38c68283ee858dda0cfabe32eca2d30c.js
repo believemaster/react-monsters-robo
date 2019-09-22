a7e219175dcfd2bab0bf228746615439dd84eb83
believemaster <believemasters@gmail.com>
2019-09-22 12:23:45 +0530

    Updates

---
+++ precache-manifest.38c68283ee858dda0cfabe32eca2d30c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960164058ee3ae97595d9ef153384374",
+    "url": "/react-monsters-robo/index.html"
+  },
+  {
+    "revision": "e0a3df0944cc5efffa13",
+    "url": "/react-monsters-robo/static/css/main.af307390.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/react-monsters-robo/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "e0a3df0944cc5efffa13",
+    "url": "/react-monsters-robo/static/js/main.7f0edcc4.chunk.js"
+  },
+  {
+    "revision": "3ba6e3ae74cf168d3d34",
+    "url": "/react-monsters-robo/static/js/runtime~main.20e3639b.js"
+  }
+]);
